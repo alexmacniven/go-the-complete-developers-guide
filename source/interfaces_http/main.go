22c4a1a238298d3bbf9540f3f071db854cfbbdc9
Alex Macniven <macniven.ap@gmail.com>
2022-06-05 15:37:14 +0100

    add custom writer implementation

--- source/interfaces_http/main.go
+++ source/interfaces_http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	// This is making a HTTP GET request.
 	// func http.Get(url string) (resp *http.Response, err error)
@@ -37,5 +39,21 @@ func main() {
 	// resp.Body.Read(bs)
 	// fmt.Println(string(bs))
 
-	io.Copy(os.Stdout, resp.Body)
+	// The Copy function of the io package provides a way of taking a type
+	// implementing the Writer interface and a type implementing the Reader
+	// interface and copying the output of the reader to the input of the
+	// writer. In a sense we're creating a channel from the Reader output to
+	// the writer input.
+
+	// As Copy accepts any type implementing the Writer interface, we can
+	// write our own types to easily work with the built-in functionality.
+	// This is where the power of interfaces comes into play.
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(p []byte) (int, error) {
+	fmt.Println(string(p))
+	fmt.Printf("Just printed %v bytes.\n", len(p))
+	return len(p), nil
 }
